e71ef72f851d57e22bd9a7a783d651244d509d8d
Edvin <91581401+EdvinRamirez@users.noreply.github.com>
2024-08-13 14:32:32 -0500

    Update pb3.js

--- problems/pb3.js
+++ problems/pb3.js
@@ -1,5 +1,38 @@
 // declare and define function here
 
+console.log(compress("ssssbbz"))
+
+function compress(string)
+{
+    newstring = ""
+    currentcount = 1;
+    currentchar = string.charAt(0)
+    
+    for (i = 0; i < string.length; i++)
+    {
+        if (string.charAt(i) == string.charAt(i+1))
+        {
+            currentcount++
+        }
+        else 
+        {
+            if (currentcount == 1)
+            {
+                newstring = newstring + currentchar
+            }
+            else
+            {
+                newstring = newstring + currentcount + currentchar
+            }
+
+            currentcount = 1
+            currentchar = string.charAt(i+1)
+        }
+        
+    }
+    
+    return newstring
+}
 
 // test cases
 // compress('ccaaatsss'); // -> '2c3at3s'
