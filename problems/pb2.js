18ad43a2753be56d007547d285dfa4ba492d8f14
Edvin <91581401+EdvinRamirez@users.noreply.github.com>
2024-08-13 14:50:35 -0500

    Update pb2.js

--- problems/pb2.js
+++ problems/pb2.js
@@ -1,5 +1,44 @@
 // declare and define function here
 
+// declare and define function here
+
+console.log(mostFrequentChar("mississippi"))
+
+function mostFrequentChar(string)
+{
+    mostchar = string.charAt(0)
+    mostcount = 1
+    currentcount = 1
+    currentinstance = string.charAt(0)
+    lastinstance = string.charAt(0)
+    for (i = 1; i < string.length; i++)
+    {
+        tempchar = string.charAt(i)
+        
+        if (tempchar != mostchar)
+        {
+        for (j = 1; j < string.length; j++)
+            {
+                if (tempchar == string.charAt(j))
+                {
+                    currentcount++
+                }
+            }
+            if (currentcount > mostcount)
+            {
+                mostcount = currentcount
+                mostchar = tempchar
+            }
+        }
+    }
+    return mostchar
+}
+
+
+
+// test cases
+// mostFrequentChar('bookeeper'); // -> 'e'
+// mostFrequentChar('mississippi'); // -> 'i'. 'i' and 's' appear with same frequency in 'mississippi' but 'i' appears first!
 
 // test cases
 // mostFrequentChar('bookeeper'); // -> 'e'
