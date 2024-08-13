ce6e94552639f2758261ba1a0f5a328f1e8bf2e8
Edvin <91581401+EdvinRamirez@users.noreply.github.com>
2024-08-13 14:05:25 -0500

    Update pb1.js

--- problems/pb1.js
+++ problems/pb1.js
@@ -1,5 +1,17 @@
 // declare and define function here
 
+let print1 = replaceSpaces('Today is a good day', '-')
+let print2 = replaceSpaces('Tomorrow will be a great day', '_')
+
+console.log(print1)
+console.log(print2)
+
+function replaceSpaces(string, char)
+{
+    return string.replaceAll(' ', char);
+}
+
+
 
 // test cases
 // replaceSpaces('Today is a good day', '-'); // -> 'Today-is-a-good-day'
